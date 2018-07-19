2d1d694ad0b200832663bfe451672664708e0667
Jhonatan <jhonatantupayachihurtado@gmail.com>
2018-07-19 16:47:14 -0500

    Primera prueba

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname+'/app/dist'))
 app.listen(process.env.PORT||3000)
 
 app.get('/*', (req, res)=>{
-    res.sendFile(path.join(__dirname , '/dist/index.html'))
+    res.sendFile(path.join(__dirname , '/app/dist/index.html'))
 })
 
 //npm install --no-progress
