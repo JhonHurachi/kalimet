fd867265e03349b037f62e833e68c168832dca1b
Jhonatan <jhonatantupayachihurtado@gmail.com>
2018-07-19 16:40:03 -0500

    Primera prueba

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-app.use(express.static(__dirname+'/dist'))
+app.use(express.static(__dirname+'/app/dist'))
 
 app.listen(process.env.PORT||3000)
 
@@ -10,3 +10,4 @@ app.get('/*', (req, res)=>{
     res.sendFile(path.join(__dirname , '/dist/index.html'))
 })
 
+//npm install --no-progress
