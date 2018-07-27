8667550a13915a5ea97cce9498d07b6b330fda20
Jhonatan <jhonatantupayachihurtado@gmail.com>
2018-07-26 21:14:55 -0500

    quinta prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'kal-root',
-  template: '<router-outlet></router-outlet>',
+  template: '<router-outlet style="height:100%"></router-outlet>',
   styleUrls: []
 })
 export class AppComponent {
